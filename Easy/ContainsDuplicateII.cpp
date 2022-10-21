d498089a7cc7375202bb297aed54a4dcb69d9343
Salvatore Spina <saske0109@gmail.com>
2022-10-21 14:11:49 +0200

    Create ContainsDuplicateII.cpp

---
+++ Easy/ContainsDuplicateII.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> hashmap;
+        for (int i = 0; i < nums.size(); i++) {
+            if (hashmap.count(nums[i]) && abs(hashmap[nums[i]] - i) <= k)
+                    return true;
+            hashmap[nums[i]] = i;
+        }
+        return false;
+    }
+};
